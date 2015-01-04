1a0aebb08faced91ac7c8e8d61f2214fed04a32c
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2015-01-04 11:05:56 -0500

    modified design

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -108,7 +108,7 @@ if (isset($_SERVER['KOHANA_ENV']))
  * - boolean  expose      set the X-Powered-By header                        FALSE
  */
 Kohana::init(array(
-	'base_url'   => '/',
+	'base_url'   => '/~pel/kohana/',
 	'index_file' => FALSE
 ));
 
