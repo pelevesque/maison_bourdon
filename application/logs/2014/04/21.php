14418ec41f1cf58957a08fb8099dadd93005ef6f
Pierre-Emmanuel Lévesque <pierre.e.levesque@gmail.com>
2015-01-06 12:07:07 -0500

    deleted logs

--- application/logs/2014/04/21.php
+++
@@ -1,5 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.'); ?>
-
-2014-04-21 03:36:10 --- EMERGENCY: ErrorException [ 4 ]: parse error, expecting `')'' ~ APPPATH/i18n/fr.php [ 28 ] in file:line
-2014-04-21 03:36:10 --- DEBUG: #0 [internal function]: Kohana_Core::shutdown_handler()
-#1 {main} in file:line
